ca9dfdd34f9917fc87d62ac31626832c009b2a1a
Natália Fonseca <nataliabf@outlook.com>
2022-02-07 20:22:34 -0300

    temp: middleware example

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import { router as userRouter } from "./routes/UserRouter.js";
 const app = express();
 app.use(express.json());
 
+app.use((request, response, next) => {
+  console.log(request.method, request.url);
+
+  next();
+});
+
 app.use("/api/user", userRouter);
 
 app.listen("3333", () => {
